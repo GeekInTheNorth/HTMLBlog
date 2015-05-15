0afe15aac6b19517b2bf8b78a66d2095487b8bda
Mark Stott <mark.stott@cascadehr.co.uk>
2015-05-15 08:22:35 +0100

    Switched to using html pages

--- blog/scripts/menu.js
+++ blog/scripts/menu.js
@@ -1,7 +1,12 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
+    var menuPath = "./data/contentlist.json";
+    var currentUrl = window.location.href;
+    if (currentUrl.toLowerCase().indexOf("/articles/") > -1)
+        menuPath = "../data/contentlist.json";
+
     $.ajax({
         type: "Get",
-        url: "./data/contentlist.json",
+        url: menuPath,
         dataType: "json",
         headers: {
             accept: 'application/json'
@@ -18,34 +23,35 @@
 function PopulateMenu(jsonData) {
     var numberOfArticles = jsonData.Articles.length;
     var categories = [];
-    
+
     var maximumArticlesToDisplay = numberOfArticles;
     if (maximumArticlesToDisplay > 5) maximumArticlesToDisplay = 5;
-    
+
     for (var loop = 0; loop < numberOfArticles; loop++) {
         if (loop <= maximumArticlesToDisplay) {
-            var title = jsonData.Articles[0].Title;
-            var fileName = jsonData.Articles[0].Filename;
+            var title = jsonData.Articles[loop].Title;
+            var fileName = jsonData.Articles[loop].Filename;
 
-            $("#menu-articles").append('<li><a href="#" onclick="javascript: ShowArticle(".data//' + fileName + '");">' + title + '</a></li>');
+            $("#menu-articles").append('<li><a href="'+GetArticlePath(fileName)+'");">' + title + '</a></li>');
         }
 
         var inArray = $.inArray(jsonData.Articles[loop].Category, categories);
         if (inArray < 0)
             categories.push(jsonData.Articles[loop].Category);
     }
-    
+
     for (var categoryLoop = 0; categoryLoop < categories.length; categoryLoop++) {
         $("#menu-categories").append('<li><a href="#" onclick="javascript: ShowArticlesInCategory(' + categories[categoryLoop] + ');">' + categories[categoryLoop] + '</a></li>');
     }
 }
 
-function ShowArticle(fileName) {
-    
-}
+function GetArticlePath(fileName) {
+    var menuPath = "./articles/" + fileName;
+    var currentUrl = window.location.href;
+    if (currentUrl.toLowerCase().indexOf("/articles/") > -1)
+        menuPath = "../articles/" + fileName;
 
-function ShowArticlesInCategory(category) {
-    
+    return menuPath;
 }
 
 function DisplayConnectionError() {
