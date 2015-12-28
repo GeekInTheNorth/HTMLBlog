d1a4a5c6434b820e0934ac92a14cbaa5af3e72f8
unknown <mypantsareontheoutside@gmail.com>
2015-12-28 10:06:54 +0000

    Added the Wedding Set List article

---
+++ blog/scripts/WeddingSetList.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function () {
+    $('table.content-list').each(function () {
+        $("tr:odd").addClass("row").removeClass("alt-row");
+        $("tr:even").addClass("alt-row").removeClass("row");
+    });
+});
