b375bd1615e53599d31d166fe2a2000caef320dd
Mark Stott <mark.stott@cascadehr.co.uk>
2016-02-07 14:55:17 +0000

    Implement Dr Who Lego article

---
+++ blog/scripts/LegoDrWho.js
@@ -0,0 +1,36 @@
+﻿$(document).ready(function () {
+    $("img#lego-image").data("data-image-position", 1);
+});
+
+function DisplayNextImage() {
+    var currentImage = $("img#lego-image").data("data-image-position");
+
+    if (currentImage >= 17)
+        currentImage = 1;
+    else
+        currentImage++;
+
+    ShowImage(currentImage);
+}
+
+function DisplayPreviousImage() {
+    var currentImage = $("img#lego-image").data("data-image-position");
+
+    if (currentImage <= 1)
+        currentImage = 17;
+    else
+        currentImage--;
+
+    ShowImage(currentImage);
+}
+
+function ShowImage(imageNumber) {
+    $("img#lego-image").data("data-image-position", imageNumber);
+
+    var fileLocation = "images/LegoDrWho";
+    if (imageNumber < 10)
+        fileLocation += "0";
+    fileLocation += imageNumber + ".jpg";
+
+    $("img#lego-image").attr("src", fileLocation);
+}
