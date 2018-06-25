44d0ed1b9b71a41a36d23fe849886b757bd383b5
Mark Stott <mark.stott@twentysixdigital.com>
2018-06-25 15:04:36 +0100

    Convert lego dr who article

--- blog/scripts/site.js
+++ blog/scripts/site.js
@@ -61,12 +61,12 @@ var summaryApp = new Vue({
             this.posts = [];
             var limit = 4;
             var articlesDisplayed = 0;
-            if (category !== "all")
+            if (category !== "Latest")
                 limit = 99;
 
             for (var index in siteSettings.Articles){
                 var article = siteSettings.Articles[index];
-                if (category === "all" || category === article.Category){
+                if (category === "Latest" || category === article.Category){
                     this.posts.push(article);
                     articlesDisplayed++;
                 }
@@ -101,6 +101,8 @@ var categoryMenuApp = new Vue({
                 if (this.categories.indexOf(category) === -1)
                     this.categories.push(category);
             }
+            this.categories.sort();
+            this.categories.unshift("Latest");
         },
         menuClick: function (category){
             summaryApp.categorySelected(category);
@@ -111,7 +113,54 @@ var categoryMenuApp = new Vue({
 function LoadSite(data){
     siteSettings = data;
     categoryMenuApp.showCategoryMenu();
-    summaryApp.categorySelected("all");
+
+    var showHomePage = true;
+    
+    var articleRequested = getUrlVar("article");
+
+    if (articleRequested !== undefined && articleRequested !== "")
+    {
+        for(var index in siteSettings.Articles){
+            var article = siteSettings.Articles[index];
+            if (article.Title.toLowerCase() === articleRequested.toLowerCase())
+            {
+                showHomePage = false;
+                articleApp.articleSelected(article.Title, article.Category, article.Filename, article.Published);
+                return;
+            }
+        }
+    }
+
+    if (showHomePage)
+        summaryApp.categorySelected("Latest");
+}
+
+function getUrlVar(varName){
+    var vars = getUrlVars();
+    var varNamelc = varName.toLowerCase();
+
+    if (vars.includes(varName.toLowerCase()))
+        return vars[varName.toLowerCase()];
+
+    return undefined;
+}
+
+function getUrlVars() {
+    var vars = [], hash;
+    var url = window.location.href;
+
+    if (url.indexOf('?') === -1)
+    {
+        return vars;
+    }
+
+    var hashes = url.slice(url.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0].toLowerCase());
+        vars[hash[0].toLowerCase()] = decodeURIComponent(hash[1]);
+    }
+    return vars;
 }
 
 $(document).ready(function () {
