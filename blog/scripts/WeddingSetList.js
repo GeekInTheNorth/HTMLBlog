f2776903048c40904866f38de9f583308d6bb231
Mark Stott <mypantsareontheoutside@gmail.com>
2018-08-02 13:08:39 +0100

    Remove old js and styles

--- blog/scripts/WeddingSetList.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-    $('table.content-list').each(function () {
-        $("tr:odd").addClass("row").removeClass("alt-row");
-        $("tr:even").addClass("alt-row").removeClass("row");
-    });
-});
